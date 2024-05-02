b4812c7bf7f1ba639f3365a7fc7aa3a9e08264eb
Harsh Poddar <harshvpoddar1605@gmail.com>
2024-05-02 11:24:22 +0530

    first commit

---
+++ hello/target/debug/deps/hello-657bd9d48094196b.d
@@ -0,0 +1,5 @@
+C:\Projects\rust\hello\target\debug\deps\hello-657bd9d48094196b.exe: src\main.rs
+
+C:\Projects\rust\hello\target\debug\deps\hello-657bd9d48094196b.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Projects\rust\hello\target\debug\hello.exe: C:\Projects\rust\hello\src\main.rs
